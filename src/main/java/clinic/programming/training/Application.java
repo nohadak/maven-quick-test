d4f0dce72f32dfa21063e26532c4097703aa51f0
akikinohad <nohad_14@hotmail.com>
2020-03-08 18:27:07 +0200

    Pom.xml file

---
+++ src/main/java/clinic/programming/training/Application.java
@@ -0,0 +1,13 @@
+package clinic.programming.training;
+
+public class Applciation {
+
+	public Application(){
+		System.out.println("Inside Application");
+	}
+
+	public static void main (String[]args){
+		System.out.println("Starting Application");
+	Application app = new Application();
+	}
+}
